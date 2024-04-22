1e8b147cd5db4002a7179f3b81e5165c41c094c3
arlohewitt <75036102+arlohewitt@users.noreply.github.com>
2024-04-22 20:49:50 +0100

    Offsets for 22/04/24

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 // Core
-constexpr uint64_t OFF_LEVEL = 0x16f9090;                         //[Miscellaneous]->LevelName
-constexpr uint64_t OFF_LOCAL_PLAYER = 0x2182bf8;                  //[Miscellaneous]->LocalPlayer
-constexpr uint64_t OFF_ENTITY_LIST = 0x1DD40F8;                   //[Miscellaneous]->cl_entitylist
+constexpr uint64_t OFF_LEVEL = 0x16f94f0;                         //[Miscellaneous]->LevelName
+constexpr uint64_t OFF_LOCAL_PLAYER = 0x21830f8;                  //[Miscellaneous]->LocalPlayer
+constexpr uint64_t OFF_ENTITY_LIST = 0x1DD45F8;                   //[Miscellaneous]->cl_entitylist
 
-constexpr uint64_t OFF_NAME_LIST = 0xC5FEC00;                     //[Miscellaneous]->NameList
+
+constexpr uint64_t OFF_NAME_LIST = 0xC5FF080;                     //[Miscellaneous]->NameList
 constexpr uint64_t OFF_NAME_INDEX = 0x38;                         //nameIndex
 
 // HUD
-constexpr uint64_t OFF_VIEWRENDER = 0x73f14f8;                    //[Miscellaneous]->ViewRender
+constexpr uint64_t OFF_VIEWRENDER = 0x73f1978;                    //[Miscellaneous]->ViewRenderer
 constexpr uint64_t OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
 
 // Player
@@ -18,10 +19,10 @@ constexpr uint64_t OFF_MAXHEALTH = 0x0460;                        //[RecvTable.D
 constexpr uint64_t OFF_SHIELD = 0x01a0;                           //[RecvTable.DT_TitanSoul]->m_shieldHealth
 constexpr uint64_t OFF_MAXSHIELD = 0x01a4;                        //[RecvTable.DT_TitanSoul]->m_shieldHealthMax
 
-constexpr uint64_t OFF_INATTACK = 0x073f2798;                     //[Buttons]->in_attack
+constexpr uint64_t OFF_INATTACK = 0x073f2c18;                     //[Buttons]->in_attack
 
 constexpr uint64_t OFF_CAMERAORIGIN = 0x1ed0;                     //[Miscellaneous]->CPlayer!camera_origin
-constexpr uint64_t OFF_STUDIOHDR = 0xff0;                        //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
+constexpr uint64_t OFF_STUDIOHDR = 0xff0;                         //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
 constexpr uint64_t OFF_BONES = 0x0da0 + 0x48;                     //m_nForceBone
 
 constexpr uint64_t OFF_LOCAL_ORIGIN = 0x017c;                     //[DataMap.C_BaseEntity]->m_vecAbsOrigin
@@ -31,7 +32,7 @@ constexpr uint64_t OFF_ZOOMING = 0x1bd1;                          //[RecvTable.D
 constexpr uint64_t OFF_TEAM_NUMBER = 0x0328;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
 constexpr uint64_t OFF_NAME = 0x0471;                             //[RecvTable.DT_BaseEntity]->m_iName
 constexpr uint64_t OFF_LIFE_STATE = 0x0680;                       //[RecvTable.DT_Player]->m_lifeState
-constexpr uint64_t OFF_BLEEDOUT_STATE = 0x26e0;                   //[RecvTable.DT_Player]->m_bleedoutState  
+constexpr uint64_t OFF_BLEEDOUT_STATE = 0x26e0;                   //[RecvTable.DT_Player]->m_bleedoutState
 constexpr uint64_t OFF_LAST_VISIBLE_TIME = 0x198d + 0x3;          //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
 constexpr uint64_t OFF_LAST_AIMEDAT_TIME = 0x198d + 0x3 + 0x8;    //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
 constexpr uint64_t OFF_VIEW_ANGLES = 0x2534 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
@@ -47,10 +48,10 @@ constexpr uint64_t OFF_OFFHAND_WEAPON = 0x1944;                   //m_latestNonO
 constexpr uint64_t OFF_CURRENTZOOMFOV = 0x15d0 + 0x00b8;          //m_playerData + m_curZoomFOV
 constexpr uint64_t OFF_TARGETZOOMFOV = 0x15d0 + 0x00bc;           //m_playerData + m_targetZoomFOV
 constexpr uint64_t OFF_WEAPON_AMMO = 0x1580;                      //RecvTable.DT_WeaponX_LocalWeaponData -> m_ammoInClip
-constexpr uint64_t OFF_RELOADING = 0x159a;						  //[RecvTable.DT_WeaponX_LocalWeaponData]-> m_bInReload
+constexpr uint64_t OFF_RELOADING = 0x159a;                        //[RecvTable.DT_WeaponX_LocalWeaponData]-> m_bInReload
 
 // Glow
-constexpr uint64_t OFF_GLOW_HIGHLIGHTS = 0xBB24350;               //HighlightSettings
+constexpr uint64_t OFF_GLOW_HIGHLIGHTS = 0xBB247D0;               //HighlightSettings
 constexpr uint64_t OFF_GLOW_ENABLE = 0x26c;                       //Script_Highlight_GetCurrentContext
 constexpr uint64_t OFF_GLOW_THROUGH_WALL = 0x26c;                 //Script_Highlight_SetVisibilityType
 constexpr uint64_t OFF_GLOW_FIX = 0x268;
